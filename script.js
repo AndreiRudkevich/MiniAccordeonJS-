fdf886238f5e006f1b5224ecf0a83f61022cbff0
Andrei Rudkevich <andrei.rudkevich93@gmail.com>
2020-11-27 20:34:24 +0300

    Болеее продвинутый вариант: Меньше HTML, больше JS

--- script.js
+++ script.js
@@ -1,57 +1,111 @@
-"use strict";
-
-var i,
-    doc = document,
-    title = doc.getElementsByClassName("accordeon__title"),
-    content = doc.getElementsByClassName("accordeon__content");
-
-for (i = 0; i < title.length; i++)
-{
-  title[i].addEventListener("click", changeClassActive, false);
-
-  if (i%2 === 0)
+const topicsData = [{
+    "topicTitle": "Основы языка и как на нём говорить правильно",
+    "topicList": [
+      "Лексическая структура",
+      "Типы данных, значения и переменные",
+      "Выражения и операторы инструкции"
+    ]
+  },
   {
-    title[i].classList.add("accordeon__title_odd");
+    "topicTitle": "Функции",
+    "topicList": [
+      "Lorem ipsum dolor sit amet.",
+      "Nostrum laboriosam quibusdam numquam asperiores!",
+      "Eveniet excepturi sunt non officia."
+    ]
+  },
+  {
+    "topicTitle": "Обработка ошибок",
+    "topicList": [
+      "Lorem ipsum dolor sit amet.",
+      "Nostrum laboriosam quibusdam numquam asperiores!",
+      "Eveniet excepturi sunt non officia."
+    ]
+  },
+  {
+    "topicTitle": "Объекты и массивы",
+    "topicList": [
+      "Lorem ipsum dolor sit amet.",
+      "Nostrum laboriosam quibusdam numquam asperiores!",
+      "Eveniet excepturi sunt non officia."
+    ]
+  },
+  {
+    "topicTitle": "События",
+    "topicList": [
+      "Lorem ipsum dolor sit amet.",
+      "Nostrum laboriosam quibusdam numquam asperiores!",
+      "Eveniet excepturi sunt non officia."
+    ]
+  },
+]
+
+let accordeon = document.querySelector("#accordeon");
+let accTitle = [];
+
+accordeon.innerHTML = `${topicsData.map(topicTemplate).join("")}`
+
+function getAccTitle() {
+  for (let i = 0; i < accordeon.childElementCount; i++) {
+    if (i % 2 === 0) {
+      accTitle.push(accordeon.children[i]);
+    }
+  }
+};
+
+getAccTitle();
+
+function topicTemplate(topic) {
+  return `
+    <div class="accordeon__title">${topic.topicTitle}
+      <div class="accordeon__polygon"></div>
+    </div>
+    <div class="accordeon__content">${topicListItems(topic.topicList)}</div>
+      `
+}
+
+function topicListItems(items) {
+  return `
+    <ul class="content__list">${items.map(item => `<li class="content__item">${item}</li>`).join("")}</ul>
+  `
+}
+
+for (let i = 0; i < accTitle.length; i++) {
+  accTitle[i].addEventListener("click", changeClassActive, false);
+  if (i % 2 === 0) {
+    accTitle[i].classList.add("accordeon__title_odd");
   }
 }
 
-function changeClassActive()
-{
-  var classes = this.getAttribute("class"),
-      newClasses = "",
-      classesArr = classes.split(" "),
-      newClassesArr = classes.split(" ");
+function changeClassActive(e) {
+  let classes = e.target.getAttribute("class"),
+    newClasses = "",
+    classesArr = classes.split(" "),
+    newClassesArr = classes.split(" ");
 
-  for (var j = 0; j < classesArr.length; j++)
-  {
-    if (classesArr[j] == "accordeon__title-active")
-    {
+  for (let j = 0; j < classesArr.length; j++) {
+    if (classesArr[j] == "accordeon__title-active") {
       classesArr.splice([j], 1);
     }
   }
-  if (classesArr.length === newClassesArr.length)
-  {
+  if (classesArr.length === newClassesArr.length) {
     classesArr.push("accordeon__title-active");
     newClasses = classesArr.join(" ");
-  } else
-  {
+  } else {
     newClasses = classesArr.join(" ");
   }
 
-  for (var l = 0; l < title.length; l++)
-  {
-    var oldClasses = title[l].getAttribute("class"),
-        oldClassesArr = oldClasses.split(" ");
-    for (var k = 0; k < oldClassesArr.length; k++)
-    {
-      if (oldClassesArr[k] == "accordeon__title-active")
-      {
+  for (let l = 0; l < accTitle.length; l++) {
+    let oldClasses = accTitle[l].getAttribute("class"),
+      oldClassesArr = oldClasses.split(" ");
+    for (let k = 0; k < oldClassesArr.length; k++) {
+      if (oldClassesArr[k] == "accordeon__title-active") {
         oldClassesArr.splice([k], 1);
       }
       oldClasses = oldClassesArr.join(" ");
-      title[l].setAttribute("class", oldClasses);
+      accTitle[l].setAttribute("class", oldClasses);
 
     }
   }
-  this.setAttribute("class", newClasses);
+  e.target.setAttribute("class", newClasses);
 }
